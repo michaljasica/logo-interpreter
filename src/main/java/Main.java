b6a9c8984e017477ed260a0022e155178292d860
michaljasica <michal.jasica98@gmail.com>
2020-06-14 18:11:20 +0200

    build

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,7 +26,8 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("scene.fxml"));
         Parent root = (Parent) loader.load();
-        Scene scene = new Scene(root, 300, 275);
+        Scene scene = new Scene(root, 950, 550);
+        stage.setResizable(false);
         ViewController controller = (ViewController) loader.getController();
 
         ExpressionListener expressionListener = new ExpressionListener();
